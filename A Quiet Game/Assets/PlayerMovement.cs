ab41519a3f27d2450b1b7c9007724ce41ce83348
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2021-12-14 00:21:44 -0800

    Adding Player Footsteps"

--- A Quiet Game/Assets/PlayerMovement.cs
+++ A Quiet Game/Assets/PlayerMovement.cs
@@ -6,23 +6,61 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] CharacterController controller;
     [SerializeField] float speed = 12f;
+    [SerializeField] float gravity = -9.81f;
+    [SerializeField] float jumpHeight = 3f;
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundDistance = 0.4f;
+    [SerializeField] LayerMask groundMask;
+
+    [SerializeField] AudioClip crouching;
+    [SerializeField] AudioClip walking;
+    [SerializeField] AudioClip running;
+
+    Vector3 velocity;
+    bool isGrounded;
+    AudioSource footsteps;
+
+    void Start(){
+        footsteps = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+    { 
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        if(isGrounded && velocity.y < 0){
+            velocity.y = -2f;
+        }
 
-        controller.Move(move * speed * Time.deltaTime);
+        if((Input.GetAxis("Vertical") != 0.0f) || (Input.GetAxis("Horizontal") != 0.0f)){
+            //play sound 
+             if (!footsteps.isPlaying)
+            {
+                footsteps.Play();
+            }
+            
+            Debug.Log("Footsteps Play");
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
 
+            Vector3 move = transform.right * x + transform.forward * z;
+
+            controller.Move(move * speed * Time.deltaTime);
+
+            if(Input.GetButtonDown("Jump") && isGrounded){
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+
+            velocity.y += gravity * Time.deltaTime;
+
+            controller.Move(velocity * Time.deltaTime);
+        } else {
+            //pause sound
+            footsteps.Pause();
+            Debug.Log("Footsteps Pause");
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # AQuietGame
+
+Created for the [Games for Blind Gamers](https://itch.io/jam/games-for-blind-gamers) Game Jam
+
+Used Unity 2020.3.22f1
